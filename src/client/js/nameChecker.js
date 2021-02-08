d193a4e1aa93c039ec5b75ee8971ee2043a02bb1
Basia Dzaman <barbbasia@hotmail.com>
2021-02-08 20:26:19 +0000

    added plugins, small fixes

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,15 +1,12 @@
 function checkForName(inputText) {
     console.log("::: Running checkForName :::", inputText);
     let names = [
-        "Picard",
-        "Janeway",
-        "Kirk",
-        "Archer",
-        "Georgiou"
+        "0000",
+        "fefefe"
     ]
 
     if(names.includes(inputText)) {
-        alert("Welcome, Captain!")
+        alert("You've guessed the password!")
     }
 }
 
