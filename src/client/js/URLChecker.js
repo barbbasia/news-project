f3d06bc01e03f4d2b629e1af8e3c6e415fa19a4f
Basia Dzaman <barbbasia@hotmail.com>
2021-02-11 14:20:53 +0000

    validating the URL input

--- src/client/js/URLChecker.js
+++ src/client/js/URLChecker.js
@@ -1,13 +1,17 @@
 function checkURL(inputURL) {
-    console.log("::: Running checkURL :::", inputURL);
-    let names = [
-        "0000",
-        "fefefe"
-    ]
+    console.log("::: Validating URL :::");
 
-    if(names.includes(inputURL)) {
-        alert("You've guessed the password!")
+    const regex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi);
+
+    if (inputURL.match(regex)) {
+        console.log(inputURL, "is a valid URL");
+        return true;
+    } else {
+        alert("Please enter a valid URL");
+        console.log(inputURL, "is NOT a valid URL");
+        return false;
     }
+
 }
 
-export { checkURL }
+export {checkURL}
